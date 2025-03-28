a6c0c59342d2ffef7282963d977ce81cd802e414
Reuben Stein <rmstein100@MacBook-Pro-4.local>
2025-03-28 14:43:48 -0500

    Fix: switch to default export for firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -6,3 +6,5 @@ export const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+export default firebaseConfig;
