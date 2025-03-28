40fd23c29575062be9d0e62acff71961c1e63966
Reuben Stein <rmstein100@MacBook-Pro-4.local>
2025-03-28 14:10:41 -0500

    Add firebaseConfig module

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2821
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 export const firebaseConfig = \{\
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,\
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,\
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,\
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,\
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,\
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,\
+\};\
+}
