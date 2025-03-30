803984790ac9e7aa7c72152c6be6ee1adc532e5f
Reuben Stein <rmstein100@MacBook-Pro-4.local>
2025-03-30 13:23:55 -0500

    🔧 Fixed Firebase config with correct Web App credentials

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDEJUtcm_cYwZo6qq5JyJL0nnMbJcRWM8",
+  apiKey: "AIzaSyDEJUtcm_cYwZo6qq5JyJL0nnMbJcRVM8",
   authDomain: "opex-test-3e44b.firebaseapp.com",
   projectId: "opex-test-3e44b",
   storageBucket: "opex-test-3e44b.appspot.com",
   messagingSenderId: "1052140461155",
-  appId: "1:1052140461155:web:bb3e057f1bc80499dcac56"
+  appId: "1:1052140461155:web:e84167b4b6f38c82dcac56"
 };
-
-console.log("🔥 Firebase Config", firebaseConfig);
 
 export default firebaseConfig;
